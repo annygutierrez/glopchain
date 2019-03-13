80798bed9435240ccd2db55efd44227aa58028f0
Anny Guttierrez <agutilop@everis.com>
2019-03-13 17:28:24 -0500

    Add description in JSDoc
    - Add description to the constructor - JSDoc

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
-  // The index is optional and it tell us where the block is on the chain
-  // The timestamp tell us when the block was created
-  // Data includes any type of data that you want to associate with the block -How much mony was transfered and who is the sender and the reciver
-  // The previous hash is a string that contains the hash of the block before this one - This is very important and ensures the importance of the blockchain
 
+  /**
+   * The index is optional and it tell us where the block is on the chain.
+   * The timestamp tell us when the block was created.
+   * Data includes any type of data that you want to associate with the block -How much mony was transfered and who is the sender and the reciver.
+   * The previous hash is a string that contains the hash of the block before this one - This is very important and ensures the importance of the blockchain.
+   * @constructs Block
+   * @param {number} index - Block number.
+   * @param {timestamp} timestamp - Creation date.
+   * @param {object} data - Attach information.
+   * @param {string} previousHash - Previous hash: Latest block hash.
+   */
   constructor(index, timestamp, data, previousHash = '') {
     this.index = index;
     this.timestamp = timestamp;
@@ -42,21 +49,21 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
-  
+
   // To verify the integrity of our blockchain
   isChainValid() {
     // We are NOT going to start with block 0 because block 0 is a genesis block
-    for(let i = 1; i < this.chain.length; i++) {
+    for (let i = 1; i < this.chain.length; i++) {
       const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i- 1];
+      const previousBlock = this.chain[i - 1];
 
       // Check if the hash of the block is still valid
-      if(currentBlock.hash !== currentBlock.calculateHash()) {
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
         return false;
       }
 
       // Check if our block point to the correct previus block
-      if(currentBlock.previousHash !== previousBlock.hash) {
+      if (currentBlock.previousHash !== previousBlock.hash) {
         return false;
       }
     }
